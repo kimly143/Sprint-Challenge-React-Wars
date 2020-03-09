493fc422f97d61700bf1214d1308a49fd9d30d8e
Kim Ly Buck <kim.3993@gmail.com>
2020-03-08 21:01:34 -0500

    clean up code, adding comments

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import Loading from './components/Loading/Loading';
@@ -7,47 +7,44 @@ import { Container } from 'reactstrap';
 import SearchBar from './components/SearchBar/SearchBar';
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
-  const [characters, setCharacters] = useState(null);
-  const [query, setQuery] = useState('');
+	// Try to think through what state you'll need for this app before starting. Then build out
+	// the state properties here.
+	const [ characters, setCharacters ] = useState(null);
+	const [ query, setQuery ] = useState('');
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
-  useEffect(
-    () => {
-      axios.get('https://swapi.co/api/people')
-        .then(response => {
-          console.log(response.data)
-          setCharacters(response.data.results);
-        })
-        .catch(error => console.error(error));  
-    },
-    []
-  );
+	// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
+	// side effect in a component, you want to think about which state and/or props it should
+	// sync up with, if any.
+	useEffect(() => {
+		axios
+			.get('https://swapi.co/api/people')
+			.then((response) => {
+				console.log(response.data);
+				setCharacters(response.data.results);
+			})
+			.catch((error) => console.error(error));
+	}, []);
+	// STRETCH: search with regular expression === Kim Buck March 8, 2020
+	const queryRX = new RegExp(query, 'i');
 
-  const queryRX = new RegExp(query, 'i');
+	return (
+		<div className='App'>
+			<h1 className='Header'>React Wars</h1>
+			<SearchBar query={query} setQuery={setQuery} />
 
-  return (
-    <div className="App">
-      <h1 className="Header">React Wars</h1>
-      <SearchBar query={query} setQuery={setQuery} />
-        
-        {!characters && <Loading />}
-
-        {characters && (<Container className='characters'>
-          { characters.filter((character)=>{
-            if (query=='') return true;
-            return queryRX.test(character.name) || queryRX.test(character.gender);
-          }).map(character => 
-          <Character characterData={character} query={query}
-          />
-        )}
-        </Container>
-        )}
-    </div>
-  );
-}
+			{!characters && <Loading />}
+			{characters && (
+				<Container className='characters'>
+          {/* STRETCH: search using filter, going throw characters and queryRX out === Kim Buck March 8, 2020 */}
+					{characters.filter((character) => {
+							if (query == '') return true;
+							return queryRX.test(character.name) || queryRX.test(character.gender);
+						})
+						.map((character) => <Character characterData={character} query={query} />)}
+				</Container>
+			)}
+		</div>
+	);
+};
 
 export default App;
