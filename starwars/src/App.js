bf35c290fd0d84a9a6a21ba506f685c7e3e35745
Kim Ly Buck <kim.3993@gmail.com>
2020-03-08 21:40:24 -0500

    all stretch goals finished

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,46 +2,47 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios';
 import Loading from './components/Loading/Loading';
-import Character from './components/Character/Character';
-import { Container, Row } from 'reactstrap';
+import Characters from './components/Characters/Characters';
 import SearchBar from './components/SearchBar/SearchBar';
 
 const App = () => {
-	// Try to think through what state you'll need for this app before starting. Then build out
-	// the state properties here.
 	const [ characters, setCharacters ] = useState(null);
 	const [ query, setQuery ] = useState('');
+	const [ nextpageUrl, setNextpageUrl ] = useState(null);
 
-	// Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
-	// side effect in a component, you want to think about which state and/or props it should
-	// sync up with, if any.
 	useEffect(() => {
 		axios
 			.get('https://swapi.co/api/people')
 			.then((response) => {
 				console.log(response.data);
 				setCharacters(response.data.results);
+				setNextpageUrl(response.data.next);
 			})
 			.catch((error) => console.error(error));
 	}, []);
-	// STRETCH: search with regular expression === Kim Buck March 8, 2020
-	const queryRX = new RegExp(query, 'i');
 
 	return (
-		<div className='App'>
-			<h1 className='Header'>React Wars</h1>
+		<div className="App">
+			<h1 className="Header">React Wars</h1>
 			<SearchBar query={query} setQuery={setQuery} />
-
 			{!characters && <Loading />}
 			{characters && (
-				<Container className='characters'><Row>
-          {/* STRETCH: search using filter, going throw characters and queryRX out === Kim Buck March 8, 2020 */}
-					{characters.filter((character) => {
-							if (query == '') return true;
-							return queryRX.test(character.name) || queryRX.test(character.gender);
-						})
-						.map((character) => <Character characterData={character} query={query} />)}
-				</Row></Container>
+				<Characters
+					characters={characters}
+					query={query}
+					onLoadMore={() => {
+						//fetch nextpage, update characters list, update nextpageUrl
+						if (nextpageUrl === null) return;
+						axios
+							.get(nextpageUrl)
+							.then((response) => {
+								console.log(response.data);
+								setCharacters(characters.concat(response.data.results));
+								setNextpageUrl(response.data.next);
+							})
+							.catch((error) => console.error(error));
+					}}
+				/>
 			)}
 		</div>
 	);
