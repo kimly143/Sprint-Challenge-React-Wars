10ad6df367ba8fef3449bdd90ab4d6751d7be1a3
Kim Ly Buck <kim.3993@gmail.com>
2020-03-08 19:33:54 -0500

    clean up spaces

--- starwars/src/components/Character/Character.js
+++ starwars/src/components/Character/Character.js
@@ -1,27 +1,18 @@
-import React, { useState , useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Character = (props) => {
-    return (
-        <div className = 'character'>
-            <h3> {props.characterData.name}</h3>
-            <p className ='height'> Heigh {props.characterData.height} </p>
-            <p className ='mass'>Mass :{props.characterData.mass}</p>
-            <p className ='hair_color'>Hair color: {props.characterData.hair_color}</p>
-
-            <p className ='skin_color'>Skin color: {props.characterData.skin_color}</p>
-
-            <p className ='eye_color'>Eye color: {props.characterData.eye_color}</p>
-
-            <p className ='birth_year'>Birth year {props.characterData.birth_year}</p>
-
-            <p className ='gender'>Gender: {props.characterData.gender}</p>
-
-            <p className ='homeworld'>Home World: {props.characterData.homeworld}</p>
-
-            <p className ='mass'>{props.characterData.mass}</p>
-
-        </div>
-
-    )
-}
+	return (
+		<div className="character">
+			<h3> {props.characterData.name}</h3>
+			<p className="height"> Heigh {props.characterData.height} </p>
+			<p className="mass">Mass :{props.characterData.mass}</p>
+			<p className="hair_color">Hair color: {props.characterData.hair_color}</p>
+			<p className="skin_color">Skin color: {props.characterData.skin_color}</p>
+			<p className="eye_color">Eye color: {props.characterData.eye_color}</p>
+			<p className="birth_year">Birth year {props.characterData.birth_year}</p>
+			<p className="gender">Gender: {props.characterData.gender}</p>
+			<p className="homeworld">Home World: {props.characterData.homeworld}</p>
+		</div>
+	);
+};
 export default Character;
